7002842807b821fd6c73bf3b533589a1c025f9f2
Eugene <tsai.evgenii@gmail.com>
2021-03-12 08:35:23 +0300

    feat: the function to check brackets has been added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsMap = new Map();
+    bracketsConfig.forEach(el => {
+        bracketsMap.set(...el);
+    });
+
+    const openingBrackets = [...bracketsMap.keys()];
+    const closingBrackets = [...bracketsMap.values()];
+
+    let stack = [];
+    for (let bracket of str) {
+        let identical = false;
+        if (closingBrackets.indexOf(bracket) > -1 && openingBrackets.indexOf(bracket) > -1) {
+            identical = true;
+        }
+
+        if (closingBrackets.indexOf(bracket) > -1) {
+            if (identical && stack.indexOf(bracket) === -1) {
+                stack.push(bracket);
+            } else {
+                let matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(bracket)];
+                if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) {
+                    return false;
+                }
+            }
+        } else {
+            stack.push(bracket);
+        }
+    }
+
+    return stack.length === 0;
+};
